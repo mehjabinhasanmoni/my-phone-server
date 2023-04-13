13ce7c84b77cace10d6e6b30c250d29672c27390
mehjabin hasan <mehjabinhasanmoni@gmail.com>
2023-04-13 23:04:08 +0600

    my-phone-server server side done

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-const express = require('express')
+const express = require('express');
+var cors = require('cors');
+const phones = require('./phones.json');
+
 const app = express()
 const port = 3000
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('My Phones Information Coming Soon Toon!!!')
 })
+
+app.get('/phones',(req, res) =>{
+ res.send(phones);
+})
+
+app.get('/phones/:id',(req, res) =>{
+
+  const id = parseInt(req.params.id);
+  
+  const phone = phones.find(phone => phone.id === id) || {};
+  console.log("Phone Id  eeee: " ,id,phone );
+  res.send(phone);
+ })
 
 app.listen(port, () => {
   console.log(`My Phone Server is Running From Port soontoon ${port}`)
